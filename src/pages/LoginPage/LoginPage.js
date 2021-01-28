d30eb07650e96fbf7b8e350f1eac2b0ca85e4976
matheusF99 <mc79846@gmail.com>
2021-01-28 07:30:35 -0300

    mobile style form

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import capeloImg from '../../assets/images/capelo.svg'
 
 // import { Container } from './styles';
 
-function LoginPage() {
+const LoginPage = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="form-content">
+      <form className="form">
+
+      </form>
+      <img className="form-image" src={capeloImg}></img>
+      <h1 className="form-title">Bem-vindo!</h1>
+
+      <div className="form-inputs">
+
+        <label className="form-label">
+          <input 
+            name="e-mail" 
+            type="e-mail" 
+            className="form-input" 
+            placeholder="E-mail"
+          />
+        </label>
+        
+      </div>
+      <div className="form-inputs">
+
+        <label className="form-label">
+          <input 
+            name="password" 
+            type="password" 
+            className="form-input" 
+            placeholder="Senha"
+          />
+        </label>
+
+      </div>
+      
     </div>
   );
 }
