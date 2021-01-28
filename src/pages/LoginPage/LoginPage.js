14bf7bb5237733a1b49d2604013a1e47f610b0a0
matheusF99 <mc79846@gmail.com>
2021-01-28 07:39:28 -0300

    mobile style form

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -6,37 +6,44 @@ import capeloImg from '../../assets/images/capelo.svg'
 const LoginPage = () => {
   return (
     <div className="form-content">
+
       <form className="form">
 
-      </form>
-      <img className="form-image" src={capeloImg}></img>
-      <h1 className="form-title">Bem-vindo!</h1>
+        <img className="form-image" src={capeloImg}></img>
+        <h1 className="form-title">Bem-vindo!</h1>
 
-      <div className="form-inputs">
+        <div className="form-inputs">
 
-        <label className="form-label">
-          <input 
-            name="e-mail" 
-            type="e-mail" 
-            className="form-input" 
-            placeholder="E-mail"
-          />
-        </label>
+          <label className="form-label">
+            <input 
+              name="e-mail" 
+              type="e-mail" 
+              className="form-input" 
+              placeholder="E-mail"
+            />
+          </label>
+          
+        </div>
+
+        <div className="form-inputs">
+
+          <label className="form-label">
+            <input 
+              name="password" 
+              type="password" 
+              className="form-input" 
+              placeholder="Senha"
+            />
+          </label>
+
+        </div>
+        <span>Esqueceu a senha?</span>
         
-      </div>
-      <div className="form-inputs">
+        <button className="form-input-bnt" type="submit">LoginPage</button>
 
-        <label className="form-label">
-          <input 
-            name="password" 
-            type="password" 
-            className="form-input" 
-            placeholder="Senha"
-          />
-        </label>
+        <span> Não possui conta? <strong>Fale conosco</strong> </span>
+      </form>
 
-      </div>
-      
     </div>
   );
 }
