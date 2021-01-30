25877e89d97783f4e5d6e5f7008c4f6f3ee56991
matheusF99 <mc79846@gmail.com>
2021-01-29 21:56:17 -0300

    mobile initialscreen style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
+import InitialPage from './pages/InitialPage/InitialPage';
 
-import LoginPage from "./pages/LoginPage/LoginPage";
+// import LoginPage from "./pages/LoginPage/LoginPage";
 
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <InitialPage />
     </div>
   );
 }
--- src/pages/InitialPage/InitialPage.js
+++ src/pages/InitialPage/InitialPage.js
@@ -4,8 +4,36 @@ import React from 'react';
 
 function InitialPage() {
   return (
-    <div>
+    <div className="">
+      <div className="div-title">
+        <h2>Horarios</h2>
+      </div>
+
+      <h1>Olá Pedriana</h1>
       
+      <div className="div-courses">
+        <p>Cursos</p>
+
+        <span>Ver Todos</span>
+
+        <div className="course">
+          <p>Sistemas de informaçoes</p>
+          <p>8 Periodo</p>
+          <p>Noturno</p>
+        </div>
+        <div className="course">
+          <p>Sistemas de informaçoes</p>
+          <p>8 Periodo</p>
+          <p>Noturno</p>
+        </div>
+      </div>
+
+      <p>Inicio</p>
+
+      <div className="div-register">
+        <i>icone</i>
+        <p>cadastrar professor</p>
+      </div>
     </div>
   );
 }
