1bab9ea2da30690d237e1bc10736f399b07da98c
matheusF99 <mc79846@gmail.com>
2021-02-10 21:47:58 -0300

    refactor: make configure card in incial page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css'
-import UserForm from './components/UserForm'
+// import './App.css'
+// import UserForm from './components/UserForm'
 // import RegisterPage from './pages/RegisterPage/RegisterPage'
 // import LoginPage from "./pages/LoginPage/LoginPage";
-// import InitialPage from './pages/InitialPage/InitialPage';
+import InitialPage from './pages/InitialPage/InitialPage';
 // import CoursePage from './pages/CoursePage/CoursePage';
 // import PeriodoPage from './pages/PeriodoPage/PeriodoPage';
 // import TeacherPage from './pages/TeacherPage/TeacherPage'
@@ -13,12 +13,12 @@ function App() {
   return (
     <div className="App">
       {/* <LoginPage /> */}
-      {/* <InitialPage /> */}
+      <InitialPage />
       {/* <CoursePage /> */}
       {/* <PeriodoPage /> */}
       {/* <TeacherPage /> */}
       {/* <RegisterPage /> */}
-      <UserForm />
+      {/* <UserForm /> */}
     </div>
   );
 }
